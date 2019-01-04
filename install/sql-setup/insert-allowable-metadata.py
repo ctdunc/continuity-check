cc4a9ea9ccc25c58809e888050335c84a1185bfa
Connor Duncan <connor.t.duncan@gmail.com>
2019-01-03 19:47:50 -0800

    added sample row to metadata setup script

--- install/sql-setup/insert-allowable-metadata.py
+++ install/sql-setup/insert-allowable-metadata.py
@@ -14,5 +14,7 @@ cursor=conn.cursor()
 
 com = "CREATE TABLE IF NOT EXISTS check_metadata (Expected_Values VARCHAR(30), Institution VARCHAR(40), Wiring VARCHAR(40), Device VARCHAR(40));"
 cursor.execute(com)
+com = "INSERT INTO check_metadata (Expected_Values, Institution, Wiring, Device) VALUES ('slac_expected_values', 'UC Berkeley', 'sample wire', 'sample dev');"
+cursor.execute(com)
 conn.commit()
 conn.close()
